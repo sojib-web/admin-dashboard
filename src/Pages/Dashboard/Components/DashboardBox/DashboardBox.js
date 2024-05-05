9687c355bd7a5841388f3643829aad3acec30ee1
Md Sojib Ali <alisojib295@gmail.com>
2024-05-05 22:52:08 +0600

    4done  commit

--- src/Pages/Dashboard/Components/DashboardBox/DashboardBox.js
+++ src/Pages/Dashboard/Components/DashboardBox/DashboardBox.js
@@ -1,8 +1,28 @@
 
 import './DashboardBox.css'
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
+import { HiDotsVertical } from "react-icons/hi";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { IoIosTimer } from "react-icons/io";
+import { useState } from 'react';
 const DashboardBox = (props) => {
+
+    const [anchorEl, setAnchorEl] = useState(null)
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
-        <div className="dashboardBox" style={{ backgroundImage: `linear-gradient(to right, ${props.color ? props.color[0] : '#000'}, ${props.color ? props.color[1] : '#ccc'})` }}>
+        <Button className="dashboardBox" style={{ backgroundImage: `linear-gradient(to right, ${props.color ? props.color[0] : '#000'}, ${props.color ? props.color[1] : '#ccc'})` }}>
+            {
+                props.grow === true ? <span className='chart'><TrendingDownIcon /></span> : <span className='chart'><TrendingUpIcon /></span>
+            }
             <div className="d-flex w-100">
                 <div className="col1">
                     <h4 className="text-white">Total User</h4>
@@ -19,7 +39,50 @@ const DashboardBox = (props) => {
 
                 </div>
             </div>
-        </div>
+
+            <div className='d-flex align-items-center w-100 bottomEle'>
+                <h6 className='text-white mb-0 mt-0'>Last Mounth</h6>
+                <div className='ml-auto'>
+                    <Button className='ml-auto toggleIcon' onClick={handleClick}><HiDotsVertical /></Button>
+
+                    <Menu
+                        className='dropDownMenus'
+                        MenuListProps={{
+                            'aria-labelledby': 'long-button',
+                        }}
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        PaperProps={{
+                            style: {
+
+                                width: '20ch',
+                            },
+                        }}
+                    >
+
+                        <MenuItem onClick={handleClose}>
+                            <div className='IconText'>                            <IoIosTimer className='mr-3 Timer' />
+                                <span className='align-items-center'>Last Day</span></div>
+                        </MenuItem>
+                        <MenuItem onClick={handleClose}>
+                            <div className='IconText'>                            <IoIosTimer className='mr-3 Timer' />
+                                <span className='align-items-center'>Last Day</span></div>
+                        </MenuItem>
+                        <MenuItem onClick={handleClose}>
+                            <div className='IconText'>                            <IoIosTimer className='mr-3 Timer' />
+                                <span className='align-items-center'>Last Day</span></div>
+                        </MenuItem>
+                        <MenuItem onClick={handleClose}>
+                            <div className='IconText'>                            <IoIosTimer className='mr-3 Timer' />
+                                <span className='align-items-center'>Last Day</span></div>
+
+                        </MenuItem>
+
+                    </Menu>
+                </div>
+            </div>
+        </Button>
 
 
     )
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,29 +1,136 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dashboard.css";
 import DashboardBox from "./Components/DashboardBox/DashboardBox"
 import { FaCircleUser } from "react-icons/fa6";
+import { HiDotsVertical } from "react-icons/hi";
 import { ImCart } from "react-icons/im";
 import { IoBagHandleSharp } from "react-icons/io5";
 import { IoStarSharp } from "react-icons/io5";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import { IoIosTimer } from "react-icons/io";
+import { Chart } from "react-google-charts";
+
+
+export const data = [
+  ["Year", "Sales", "Expenses"],
+  ["2013", 1000, 400],
+  ["2014", 1170, 460],
+  ["2015", 660, 1120],
+  ["2016", 1030, 540],
+];
+
+export const options = {
+  title: "Company Performance",
+  hAxis: { title: "Year", titleTextStyle: { color: "#333" } },
+  vAxis: { minValue: 0 },
+  chartArea: { width: "50%", height: "70%" },
+};
 const Dashboard = () => {
+
+  const [anchorEl1, setAnchorEl1] = useState(null)
+  const open = Boolean(anchorEl1);
+  const handleClick = (event) => {
+    setAnchorEl1(event.currentTarget);
+  };
+  const handleCloses = () => {
+    setAnchorEl1(null);
+  };
   return (
     <>
       <div className="right-content w-100">
         <div className="row dashboardBoxWrapperRow">
           <div className="col-md-8">
             <div className="dashboardBoxWrapper d-flex">
-              <DashboardBox color={['#11998e', '#38ef7d']} icon={<FaCircleUser />} />
+              <DashboardBox color={['#11998e', '#38ef7d']} icon={<FaCircleUser />}
+                grow={true} />
               <DashboardBox color={['#6a11cb', '#2575fc']} icon={<ImCart />} />
               <DashboardBox color={['#f7971e', '#ffd200']} icon={<IoBagHandleSharp />} />
               <DashboardBox color={['#ff6e7f', '#bfe9ff']} icon={<IoStarSharp />} />
             </div>
           </div>
           <div className="col-md-3 pl-0">
-            <div className="box">
+            <div className="box graphBox">
+              <div className='d-flex align-items-center w-100 bottomEle'>
+                <h6 className='text-white mb-0 mt-0'>Total Seles</h6>
+                <div className='ml-auto'>
+                  <Button className='ml-auto toggleIcon' onClick={handleClick}><HiDotsVertical /></Button>
 
+                  <Menu
+                    className='dropDownMenus'
+                    MenuListProps={{
+                      'aria-labelledby': 'long-button',
+                    }}
+                    anchorEl={anchorEl1}
+                    open={open}
+                    onClose={handleCloses}
+                    PaperProps={{
+                      style: {
+
+                        width: '20ch',
+                      },
+                    }}
+                  >
+
+                    <MenuItem onClick={handleCloses}>
+                      <div className='IconText'>                            <IoIosTimer className='mr-3 Timer' />
+                        <span className='align-items-center'>Last</span></div>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloses}>
+                      <div className='IconText'>                            <IoIosTimer className='mr-3 Timer' />
+                        <span className='align-items-center'>Last Day</span></div>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloses}>
+                      <div className='IconText'>                            <IoIosTimer className='mr-3 Timer' />
+                        <span className='align-items-center'>Last Day</span></div>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloses}>
+                      <div className='IconText'>                            <IoIosTimer className='mr-3 Timer' />
+                        <span className='align-items-center'>Last Day</span></div>
+
+                    </MenuItem>
+
+                  </Menu>
+                </div>
+              </div>
+
+              <h3 className="text-white font-weight-bold">$3359</h3>
+              <p>834230 in last month</p>
+              <Chart
+                chartType="AreaChart"
+                width="100%"
+                height="290px"
+                data={data}
+                options={options}
+              />
             </div>
           </div>
         </div>
+
+        <div className="card shadow border-0 p-3 mt-4">
+          <h3 className="hd">Best Selling products</h3>
+
+          <div className="row">
+            <div className="col">
+              <h4>SHOW BY</h4>
+            </div>
+            <div className="col">
+              <h4>SHOW BY</h4>
+            </div>
+            <div className="col">
+              <h4>SHOW BY</h4>
+            </div>
+            <div className="col">
+              <h4>SHOW BY</h4>
+            </div>
+            <div className="col">
+              <h4>SHOW BY</h4>
+            </div>
+          </div>
+        </div>
+
+
       </div>
     </>
   );
