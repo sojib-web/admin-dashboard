9a6c4a618dd7754408d76dab73083d40ca34e8e5
Md Sojib Ali <alisojib295@gmail.com>
2024-05-06 23:06:25 +0600

    5 vi time 38  commit

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -11,8 +11,13 @@ import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
 import { IoIosTimer } from "react-icons/io";
 import { Chart } from "react-google-charts";
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import { MdRemoveRedEye } from "react-icons/md";
+import { BsPencilFill } from "react-icons/bs";
+import { MdAutoDelete } from "react-icons/md";
 
-
+import ImgProduct from "../../assets/images/12.jpg"
 export const data = [
   ["Year", "Sales", "Expenses"],
   ["2013", 1000, 400],
@@ -28,7 +33,10 @@ export const options = {
   chartArea: { width: "50%", height: "70%" },
 };
 const Dashboard = () => {
-
+  const [ShowBy, setShowBy] = useState('');
+  const [ShowByCat, setShowByCat] = useState('');
+  const [ShowByBrand, setShowByBrand] = useState('');
+  const [ShowBySearch, setShowBySearch] = useState('');
   const [anchorEl1, setAnchorEl1] = useState(null)
   const open = Boolean(anchorEl1);
   const handleClick = (event) => {
@@ -36,6 +44,22 @@ const Dashboard = () => {
   };
   const handleCloses = () => {
     setAnchorEl1(null);
+  };
+
+
+  const handleChange = (event) => {
+    setShowBy(event.target.value);
+  };
+
+  const handleChangeCat = (event) => {
+    setShowByCat(event.target.value);
+  };
+
+  const handleChangeCatBrand = (event) => {
+    setShowByBrand(event.target.value);
+  };
+  const handleChangesetShowBySearch = (event) => {
+    setShowBySearch(event.target.value);
   };
   return (
     <>
@@ -111,21 +135,141 @@ const Dashboard = () => {
         <div className="card shadow border-0 p-3 mt-4">
           <h3 className="hd">Best Selling products</h3>
 
-          <div className="row">
-            <div className="col">
-              <h4>SHOW BY</h4>
+          <div className="row cardFilters mt-3">
+            <div className="col-md-3">
+              <h4 className="texts">Show By</h4>
+              <FormControl size="small" className="w-100">
+                <Select
+                  value={ShowBy}
+                  onChange={handleChange}
+                  displayEmpty
+                  inputProps={{ 'aria-label': 'Without label' }}
+                  className="w-100"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
             </div>
-            <div className="col">
-              <h4>SHOW BY</h4>
+
+            <div className="col-md-3">
+              <h4 className="texts">Category By </h4>
+              <FormControl size="small" className="w-100">
+                <Select
+                  value={ShowByCat}
+                  onChange={handleChangeCat}
+                  displayEmpty
+                  inputProps={{ 'aria-label': 'Without label' }}
+                  className="w-100"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>10</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
             </div>
-            <div className="col">
-              <h4>SHOW BY</h4>
+
+
+            <div className="col-md-3">
+              <h4 className="texts">Brand By </h4>
+              <FormControl size="small" className="w-100">
+                <Select
+                  value={ShowByBrand}
+                  onChange={handleChangeCatBrand}
+                  displayEmpty
+                  inputProps={{ 'aria-label': 'Without label' }}
+                  className="w-100"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>10</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
             </div>
-            <div className="col">
-              <h4>SHOW BY</h4>
+
+
+            <div className="col-md-3">
+              <h4 className="texts">Brand By </h4>
+              <FormControl size="small" className="w-100">
+                <Select
+                  value={ShowBySearch}
+                  onChange={handleChangesetShowBySearch}
+                  displayEmpty
+                  inputProps={{ 'aria-label': 'Without label' }}
+                  className="w-100"
+                >
+                  <MenuItem value="">
+                    <em>Id/Name/Category/Brand</em>
+                  </MenuItem>
+                  <MenuItem value={10}>10</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
             </div>
-            <div className="col">
-              <h4>SHOW BY</h4>
+            <div className="table-responsive mt-3">
+              <table className="table table-bordered t-aling">
+                <thead className="thead-dark">
+                  <tr>
+                    <th>UID</th>
+                    <th>PRODUCT</th>
+                    <th>CATEGORY</th>
+                    <th>BRAND</th>
+                    <th>PRICE</th>
+                    <th>STOCK</th>
+                    <th>RATING</th>
+                    <th>ORDER</th>
+                    <th>SELES</th>
+                    <th>ACTION</th>
+
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>#1</td>
+                    <td>rhuiort</td>
+                    <td>
+                      <div className="d-flex productBox">
+                        <div className="info">
+                          <div className="imgWrapper">
+                            <div className="img w-100">
+                              <img src={ImgProduct} alt="" />
+                            </div>
+                          </div>
+                          <h6>means that your application is trying to import a module</h6>
+
+                          <p> If you're sure the module name and installation</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td>womens</td>
+                    <td>ricman</td>
+                    <td><del className="oldPrice">$045</del>
+                      <span className="NewPrice text-danger">$045</span></td>
+                    <td>35.(23)</td>
+                    <td>300</td>
+                    <td>$45k</td>
+                    <td>
+                      <div className="actions d-flex align-items-center">
+                        <Button className="secondary" color="secondary"><MdRemoveRedEye /></Button>
+                        <Button className="success" color="success"><BsPencilFill /></Button>
+                        <Button className="error" color="error"><MdAutoDelete /></Button>
+                      </div>
+                    </td>
+
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
